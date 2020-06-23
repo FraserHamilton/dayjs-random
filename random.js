fcaa918b879527a7273409f73b45479e69220f19
fh21 <fh21@hw.ac.uk>
2020-06-23 16:17:16 +0100

    added factory

--- random.js
+++ random.js
@@ -1,6 +1,6 @@
 const dayjs = require('dayjs')
 
-exports.extend = (option, dayjsClass, dayjsFactory) => {
+exports.extend = (dayjsFactory) => {
   dayjsFactory.between = (from, to) => {
     const fromMilli = dayjs(from).valueOf()
     const max = dayjs(to).valueOf() - fromMilli
@@ -13,14 +13,30 @@ exports.extend = (option, dayjsClass, dayjsFactory) => {
   }
 
   dayjsFactory.soon = (days = 1, refDate = dayjs()) => {
-    const to = refDate.add(days, 'day')
+    const ref = dayjs(refDate)
+    const to = ref.add(days, 'day')
 
-    return dayjs.between(refDate, to)
+    return dayjs.between(ref, to)
   }
 
   dayjsFactory.recent = (days = 1, refDate = dayjs()) => {
-    const from = refDate.subtract(days, 'day')
+    const ref = dayjs(refDate)
+    const from = ref.subtract(days, 'day')
 
-    return dayjs.between(from, refDate)
+    return dayjs.between(from, ref)
+  }
+
+  dayjsFactory.future = (years = 1, refDate = dayjs()) => {
+    const ref = dayjs(refDate)
+    const to = ref.add(years, 'year')
+
+    return dayjs.between(ref, to)
+  }
+
+  dayjsFactory.past = (years = 1, refDate = dayjs()) => {
+    const ref = dayjs(refDate)
+    const from = ref.subtract(years, 'year')
+
+    return dayjs.between(from, ref)
   }
 }
